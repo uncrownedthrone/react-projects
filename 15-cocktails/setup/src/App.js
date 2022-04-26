5efbd78b77c579b1e32035f92568adcf200b8888
Justin Swiger <justin.swiger@me.com>
2022-04-26 13:18:52 -0400

    completed cocktails proj

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import pages
+
 import Home from './pages/Home'
 import About from './pages/About'
 import SingleCocktail from './pages/SingleCocktail'
 import Error from './pages/Error'
-// import components
+
 import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div>
-      <h2>app component</h2>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/cocktail/:id'>
+            <SingleCocktail />
+          </Route>
+          <Route path='/error'>
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sint
+        suscipit facere eos alias, autem ducimus qui voluptatum molestias
+        maiores sunt, excepturi quas ratione minima error nobis corrupti
+        voluptatibus reiciendis.
+      </p>
+    </section>
   )
 }
 
