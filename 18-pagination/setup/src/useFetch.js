3a65481eacfef56e67fe0ec8107a7541993bcb78
Justin Swiger <justin.swiger@me.com>
2022-04-27 12:42:33 -0400

    completed 18 pagination proj

--- 18-pagination/setup/src/useFetch.js
+++ 18-pagination/setup/src/useFetch.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
+
 import paginate from './utils'
+
 const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
 
 export const useFetch = () => {
@@ -9,12 +11,13 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url)
     const data = await response.json()
-    setData(data)
+    setData(paginate(data))
     setLoading(false)
   }
 
   useEffect(() => {
     getProducts()
   }, [])
+
   return { loading, data }
 }
