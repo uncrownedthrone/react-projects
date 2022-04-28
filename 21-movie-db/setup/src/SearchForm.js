cf6c02c3d43682fd2ec6a5a5e976c09d56ca6ceb
Justin Swiger <justin.swiger@me.com>
2022-04-28 15:53:58 -0400

    completed 21 moviedb proj

--- 21-movie-db/setup/src/SearchForm.js
+++ 21-movie-db/setup/src/SearchForm.js
@@ -1,7 +1,26 @@
 import React from 'react'
+
 import { useGlobalContext } from './context'
+
 const SearchForm = () => {
-  return <h2>search component</h2>
+  const { query, setQuery, error } = useGlobalContext()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form className='search-form' onSubmit={handleSubmit}>
+      <h2>search for a movie</h2>
+      <input
+        type='text'
+        className='form-input'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className='error'>{error.msg}</div>}
+    </form>
+  )
 }
 
 export default SearchForm
